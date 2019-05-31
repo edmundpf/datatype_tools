0a52fe7f9d0abaab3b09f861e4eb61ec530b334e
Edmund Pfeil <edmundpf@buffalo.edu>
2019-05-31 13:59:19 -0400

    Edited setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="datatype-tools-edmundpf",
+	name="datatype-tools",
 	version="0.0.1",
 	author="Edmund Pfeil",
 	author_email="edmundpf@buffalo.edu",
