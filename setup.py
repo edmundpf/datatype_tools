16c7989dad84568e9402c158a57c0322a07e58ae
Edmund Pfeil <edmundpf@buffalo.edu>
2019-05-31 14:22:44 -0400

    Travis yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="datatype-tools",
-	version="0.0.1",
+	version="0.0.2",
 	author="Edmund Pfeil",
 	author_email="edmundpf@buffalo.edu",
 	description="Additional methods for Python's immutable data types.",
