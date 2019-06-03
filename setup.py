902e2013cacdc45d7fe053bdb3573c3c75bfb0ee
Edmund Pfeil <edmundpf@buffalo.edu>
2019-06-03 07:30:36 -0400

    PyPi v3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="datatype-tools",
-	version="0.0.2",
+	version="0.0.3",
 	author="Edmund Pfeil",
 	author_email="edmundpf@buffalo.edu",
 	description="Additional methods for Python's immutable data types.",
